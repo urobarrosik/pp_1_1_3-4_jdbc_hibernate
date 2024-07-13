1a442c3168ef48ca38a8177bad2122c58f44d2ea
urobarrosik <vadimqwedcxzas@mail.ru>
2024-07-13 16:23:54 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
